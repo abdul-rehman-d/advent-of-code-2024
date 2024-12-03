fe4afcbedf6ad6f26907d995287c5dfbf4032873
Abdul Rehman <ar618211@gmail.com>
2024-12-03 17:42:14 +0500

    added day3b question and test

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 const (
-	data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	dataA = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	dataB = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 )
 
 func TestPartA(t *testing.T) {
 	expected := 161
-	result := PartA(data)
+	result := PartA(dataA)
 
 	if expected != result {
 		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
@@ -18,8 +19,8 @@ func TestPartA(t *testing.T) {
 }
 
 func TestPartB(t *testing.T) {
-	expected := 0
-	result := PartB(data)
+	expected := 48
+	result := PartB(dataB)
 
 	if expected != result {
 		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
