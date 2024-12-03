9a94f61984c2900422e24af4750dd432a830641a
Abdul Rehman <ar618211@gmail.com>
2024-12-03 14:43:51 +0500

    day2 b done

--- day2/b.go
+++ day2/b.go
@@ -1,5 +1,38 @@
 package day2
 
+import (
+	"advent-of-code-2024/utils"
+	"slices"
+	"strings"
+)
+
+func deleteIndex(array []int, index int) []int {
+	return slices.Concat(array[:index], array[index+1:])
+}
+
 func PartB(data string) int {
-	return 0
+	lines := strings.Split(data, "\n")
+	lines = utils.FilterEmptyLines(lines)
+
+	safe := 0
+
+	for _, report := range parse(lines) {
+		if len(report) <= 1 {
+			panic("why only one value")
+		}
+
+		if check(report) {
+			safe++
+			continue
+		}
+
+		for i := 0; i < len(report); i++ {
+			if check(deleteIndex(report, i)) {
+				safe++
+				break
+			}
+		}
+	}
+
+	return safe
 }
