83727eea1e6083e619616a8e313c0b8e5ec3e801
Abdul Rehman <ar618211@gmail.com>
2024-12-04 11:33:20 +0500

    day4 generated

---
+++ day4/day4_test.go
@@ -0,0 +1,37 @@
+package day4
+
+import (
+	"testing"
+)
+
+const (
+	data = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`
+)
+
+func TestPartA(t *testing.T) {
+	expected := 18
+	result := PartA(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	expected := 0
+	result := PartB(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
