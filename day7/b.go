d4862fffbd1315a0abde6458e21340b654f1b187
Abdul Rehman <ar618211@gmail.com>
2024-12-08 21:45:46 +0500

    day7 b done

--- day7/b.go
+++ day7/b.go
@@ -1,5 +1,63 @@
 package day7
 
+import (
+	"advent-of-code-2024/utils"
+	"math"
+	"strings"
+)
+
+func getAllCombinationsB(n int) []string {
+	characters := []rune{'+', '*', '|'}
+	var results []string
+
+	var combine func(current string, depth int)
+	combine = func(current string, depth int) {
+		if depth == n {
+			results = append(results, current)
+			return
+		}
+		for _, char := range characters {
+			combine(current+string(char), depth+1)
+		}
+	}
+
+	combine("", 0)
+	return results
+}
+
 func PartB(data string) int {
-	return 0
+	lines := strings.Split(data, "\n")
+	lines = utils.FilterEmptyLines(lines)
+
+	rows := parse(lines)
+
+	answer := 0
+
+	for _, row := range rows {
+		allCombinations := getAllCombinationsB(len(row.Nums) - 1)
+
+		for _, combo := range allCombinations {
+			ans := row.Nums[0]
+			for i := 1; i < len(row.Nums); i++ {
+				switch combo[i-1] {
+				case '+':
+					ans += row.Nums[i]
+				case '*':
+					ans *= row.Nums[i]
+				case '|':
+					numOfDigit := math.Floor(math.Log10(float64(row.Nums[i]))) + 1
+					for i := 0; i < int(numOfDigit); i++ {
+						ans *= 10
+					}
+					ans += row.Nums[i]
+				}
+			}
+			if ans == row.Ans {
+				answer += row.Ans
+				break
+			}
+		}
+	}
+
+	return answer
 }
