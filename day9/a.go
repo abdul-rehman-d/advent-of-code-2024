f8ada35851165e0704b33f5bdb9bb49df973d39c
Abdul Rehman <ar618211@gmail.com>
2024-12-09 14:00:56 +0500

    day9 a done

--- day9/a.go
+++ day9/a.go
@@ -1,5 +1,55 @@
 package day9
 
+import (
+	"strings"
+)
+
 func PartA(data string) int {
-	return 0
+	data = strings.TrimSpace(data)
+	memory := []int{}
+
+	id := 0
+	for i := 0; i < len(data); i++ {
+		num := int(data[i] - '0')
+		if i%2 == 0 {
+			// file
+			for j := 0; j < num; j++ {
+				memory = append(memory, id)
+			}
+			id++
+		} else {
+			// free space
+			for j := 0; j < num; j++ {
+				memory = append(memory, -1)
+			}
+		}
+	}
+
+	i, j := 0, len(memory)-1
+	for i < j {
+		if memory[i] != -1 {
+			i++
+			continue
+		}
+		if memory[j] == -1 {
+			j--
+			continue
+		}
+		memory[i] = memory[j]
+		memory[j] = -1
+		i++
+		j--
+	}
+
+	answer := 0
+
+	for i := 0; i < len(memory); i++ {
+		if memory[i] == -1 {
+			break
+		}
+
+		answer += memory[i] * i
+	}
+
+	return answer
 }
