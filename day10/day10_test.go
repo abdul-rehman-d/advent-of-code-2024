054127304a8bdf55d3081756e8537035cf2f9fca
Abdul Rehman <ar618211@gmail.com>
2024-12-10 14:55:57 +0500

    day10 a added

---
+++ day10/day10_test.go
@@ -0,0 +1,35 @@
+package day10
+
+import (
+	"testing"
+)
+
+const (
+	data = `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+`
+)
+
+func TestPartA(t *testing.T) {
+	expected := 36
+	result := PartA(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	expected := 0
+	result := PartB(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
