e96d74683bd567389b7684189e916639c9cb4e3d
Abdul Rehman <ar618211@gmail.com>
2024-12-10 16:28:52 +0500

    day10 b done

--- day10/b.go
+++ day10/b.go
@@ -1,5 +1,57 @@
 package day10
 
+import (
+	"advent-of-code-2024/utils"
+	"fmt"
+)
+
 func PartB(data string) int {
-	return 0
+	mapp := utils.MakeIntMatrix(data)
+
+	fmt.Println(mapp)
+
+	allStartingPoints := getAll(mapp, 0)
+	// maxScoreForEach := len(getAll(mapp, 9))
+
+	ans := 0
+
+	for _, sp := range allStartingPoints {
+		stack := []Coord{sp}
+		score := map[Coord]int{}
+		for len(stack) != 0 {
+			fmt.Println("stack>", stack)
+			fmt.Println("score>", score)
+			curr := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			currHeight := mapp[curr.Row][curr.Col]
+			nextHeight := currHeight + 1
+			dirs := utils.GetDirections()
+
+			for _, dir := range dirs {
+				nextCoord := curr.Plus(dir)
+				if nextCoord.Row < 0 || nextCoord.Row >= len(mapp) {
+					continue
+				}
+				if nextCoord.Col < 0 || nextCoord.Col >= len(mapp[nextCoord.Row]) {
+					continue
+				}
+
+				if nextHeight == mapp[nextCoord.Row][nextCoord.Col] {
+					if nextHeight == 9 {
+						ans++
+						if _, ok := score[nextCoord]; !ok {
+							score[nextCoord] = 1
+						}
+						continue
+					}
+					// happy case
+
+					stack = append(stack, nextCoord)
+				}
+			}
+		}
+
+	}
+
+	return ans
 }
