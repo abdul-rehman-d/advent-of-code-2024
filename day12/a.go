25d5a689690adaa1475afa09d61691f218187b4e
Abdul Rehman <ar618211@gmail.com>
2024-12-17 20:15:22 +0500

    day 12 a done

--- day12/a.go
+++ day12/a.go
@@ -2,7 +2,7 @@ package day12
 
 import (
 	"advent-of-code-2024/utils"
-	"fmt"
+	// "fmt"
 	"strings"
 )
 
@@ -17,40 +17,30 @@ func PartA(data string) int {
 	lines := strings.Split(data, "\n")
 	lines = utils.FilterEmptyLines(lines)
 
-	// mapp := map[byte][]Coord{}
-	// seen := map[Coord]bool{}
-	// allCharacters := map[byte]Coord{}
-
-	// startingPoint := Coord{
-	// 	Row: 0,
-	// 	Col: 0,
-	// }
-	// current := lines[startingPoint.Row][startingPoint.Col]
-
 	areaMap := make([][]int, len(lines))
+	// bigTemp := make([][]byte, len(lines))
 	areaMapReverse := map[int][]Coord{}
 	toCheck := map[Coord]bool{}
 
 	for row, line := range lines {
+		temp := make([]int, len(line))
+		// temp2 := make([]byte, len(line))
 		for col := 0; col < len(line); col++ {
+			temp[col] = -1
+			// temp2[col] = '.'
 			toCheck[Coord{
 				Row: row,
 				Col: col,
 			}] = true
 		}
+		areaMap[row] = temp
+		// bigTemp[row] = temp2
 	}
 
-	// id := 0
-	//
-	// getAreaId := func() int {
-	// 	id++
-	// 	return id
-	// }
-	//
-	var findAll func(Coord, int)
-	findAll = func(current Coord, area int) {
+	var findAll func(Coord, int) bool
+	findAll = func(current Coord, area int) bool {
 		if _, has := toCheck[current]; !has {
-			return
+			return false
 		}
 		currentCharacter := lines[current.Row][current.Col]
 		if _, ok := areaMapReverse[area]; ok {
@@ -58,119 +48,98 @@ func PartA(data string) int {
 		} else {
 			areaMapReverse[area] = []Coord{current}
 		}
+
+		areaMap[current.Row][current.Col] = area
+		// bigTemp[current.Row][current.Col] = currentCharacter
 		delete(toCheck, current)
+
 		for _, dir := range utils.GetDirections() {
 			next := current.Plus(dir)
-			if next.Row < 0 || next.Row >= len(areaMap) {
+			if next.Row < 0 || next.Row >= len(lines) {
 				continue
 			}
-			if next.Col < 0 || next.Col >= len(areaMap[next.Row]) {
+			if next.Col < 0 || next.Col >= len(lines[next.Row]) {
 				continue
 			}
 
 			nextCharacter := lines[next.Row][next.Col]
 			if nextCharacter == currentCharacter {
 				findAll(next, area)
-			} else {
-				findAll(next, area+1)
 			}
 		}
 
+		return true
+
 	}
+
+	area := 0
 	for coord := range toCheck {
-		findAll(coord, 0)
-	}
-	// for row, line := range lines {
-	// 	temp := make([]int, len(line))
-	// 	areaMap[row] = temp
-	//
-	// 	col := 0
-	//
-	// 	for {
-	// 		current := line[col]
-	//
-	// 		// up
-	// 		if row != 0 && lines[row-1][col] == current {
-	// 			temp[col] = areaMap[row-1][col]
-	// 			continue
-	// 		}
-	//
-	// 		// left
-	// 		if col != 0 && lines[row][col-1] == current {
-	// 			temp[col] = temp[col-1]
-	// 			continue
-	// 		}
-	//
-	// 		// new area
-	// 		temp[col] = id
-	// 		id++
-	// 	}
-	// }
-
-	// for row := range areaMap {
-	// 	for col := range areaMap[row] {
-	// 		area := areaMap[row][col]
-	// 		if _, ok := areaMapReverse[area]; ok {
-	// 			areaMapReverse[area] = append(areaMapReverse[area], Coord{
-	// 				Row: row,
-	// 				Col: col,
-	// 			})
-	// 		} else {
-	// 			areaMapReverse[area] = []Coord{{
-	// 				Row: row,
-	// 				Col: col,
-	// 			}}
-	// 		}
-	// 	}
-	// }
-	//
-	// answer := 0
-	// solutions := make([]Sol, len(areaMapReverse))
-	//
-	// for area, coords := range areaMapReverse {
-	// 	if len(coords) == 1 {
-	// 		solutions[area] = Sol{4, 1}
-	// 	} else {
-	// 		solutions[area] = Sol{0, len(coords)}
-	// 		perimeter := 0
-	//
-	// 		for _, coord := range coords {
-	// 			sides := 4
-	// 			for _, dir := range utils.GetDirections() {
-	// 				neightbour := coord.Plus(dir)
-	// 				if neightbour.Row < 0 || neightbour.Row >= len(areaMap) {
-	// 					continue
-	// 				}
-	// 				if neightbour.Col < 0 || neightbour.Col >= len(areaMap[neightbour.Row]) {
-	// 					continue
-	// 				}
-	// 				if area == areaMap[neightbour.Row][neightbour.Col] {
-	// 					sides--
-	// 				}
-	// 			}
-	// 			perimeter += sides
-	// 		}
-	// 		solutions[area].P = perimeter
-	// 	}
-	// 	answer += (solutions[area].A * solutions[area].P)
-	// }
-	//
-	for _, line := range lines {
-		fmt.Println(strings.Split(line, ""))
-	}
-	fmt.Println()
-	for _, line := range areaMap {
-		fmt.Println(line)
-	}
-	fmt.Println()
-	for _, line := range areaMapReverse {
-		character := lines[line[0].Row][line[0].Col]
-		fmt.Println(string(character), line)
-		// fmt.Println(string(character), area, ">>>>>>>>>>>>", solutions[area])
+		if findAll(coord, area) {
+			area++
+		}
 	}
 
-	fmt.Println()
-	fmt.Println()
+	answer := 0
+	solutions := make([]Sol, len(areaMapReverse))
 
-	return 0
+	for area, coords := range areaMapReverse {
+		if len(coords) == 1 {
+			solutions[area] = Sol{4, 1}
+		} else {
+			solutions[area] = Sol{0, len(coords)}
+			perimeter := 0
+
+			for _, coord := range coords {
+				sides := 4
+				for _, dir := range utils.GetDirections() {
+					neightbour := coord.Plus(dir)
+					if neightbour.Row < 0 || neightbour.Row >= len(areaMap) {
+						continue
+					}
+					if neightbour.Col < 0 || neightbour.Col >= len(areaMap[neightbour.Row]) {
+						continue
+					}
+					if area == areaMap[neightbour.Row][neightbour.Col] {
+						sides--
+					}
+				}
+				perimeter += sides
+			}
+			solutions[area].P = perimeter
+		}
+		answer += (solutions[area].A * solutions[area].P)
+	}
+
+	// fmt.Println("DATA=>")
+	// for _, line := range lines {
+	// 	fmt.Println(strings.Split(line, ""))
+	// }
+	// fmt.Println()
+	// fmt.Println("VERIFY DATA=>")
+	// for _, line := range bigTemp {
+	// 	fmt.Print("[ ")
+	// 	for _, ch := range line {
+	// 		fmt.Printf("%s ", string(ch))
+	// 	}
+	// 	fmt.Println("]")
+	// }
+	// fmt.Println()
+	// fmt.Println("AREA MAP=>")
+	// for _, line := range areaMap {
+	// 	fmt.Println(line)
+	// }
+	// fmt.Println()
+	// fmt.Println("AREA MAP REVERSE=>")
+	// for _, line := range areaMapReverse {
+	// 	character := lines[line[0].Row][line[0].Col]
+	// 	fmt.Println(string(character), line)
+	// 	// fmt.Println(string(character), area, ">>>>>>>>>>>>", solutions[area])
+	// }
+
+	// fmt.Println()
+	// fmt.Println(answer)
+	// fmt.Println()
+	// fmt.Println()
+
+	return answer
 }
